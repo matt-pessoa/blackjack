1a3905a3205e64b1989fdebc4ae29d339a84b08d
Matheus Pessoa <matheuspessoafonseca@gmail.com>
2021-09-15 09:58:36 -0300

    Declara a variavel message que armazena uma string de acordo com a condiçao

--- index.js
+++ index.js
@@ -10,12 +10,14 @@ let sum = firstCard + secondCard;
 let hasBlackJack = false;
 let isAlive = true;
 
+let message = '';
+
 if (sum <= 20) {
-  console.log("Do you want to draw a new card?");
+  message = 'Do you want to draw a new card?';
 } else if (sum === 21) {
-  console.log("Wohoo! You've got Blackjack!");
+  message = "Wohoo! You've got Blackjack!";
   hasBlackJack = true;
 } else {
-  console.log("You're out of the game!");
+  message = "You're out of the game!";
   isAlive = false;
 }
