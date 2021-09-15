8197d6803e8a3e43fc7b3864a9517e8634e87ae3
Matheus Pessoa <matheuspessoafonseca@gmail.com>
2021-09-15 10:37:00 -0300

    Remove emoçoes das mensagens

--- index.js
+++ index.js
@@ -19,12 +19,12 @@ startBtn.addEventListener('click', startGame)
 
 function startGame() {
     if (sum <= 20) {
-        message = "Do you want to draw a new card? 🙂"
+        message = "Do you want to draw a new card?"
     } else if (sum === 21) {
-        message = "Wohoo! You've got Blackjack! 🥳"
+        message = "You've got Blackjack!"
         hasBlackJack = true
     } else {
-        message = "You're out of the game! 😭"
+        message = "You're out of the game!"
         isAlive = false
     }
     // 2. Display the message in the messageEl using messageEl.textContent
