1ecee21bedaa30cb47d386252bcbd80fae2e8660
Matheus Pessoa <matheuspessoafonseca@gmail.com>
2021-09-15 10:43:54 -0300

    Display o valor da soma em HTML

--- index.js
+++ index.js
@@ -14,20 +14,23 @@ let message = '';
 
 const startBtn = document.getElementById('start-btn');
 const messageEl = document.querySelector('#message-el');
+const sumEl = document.getElementById('sum-el');
 
-startBtn.addEventListener('click', startGame)
+startBtn.addEventListener('click', startGame);
 
 function startGame() {
-    if (sum <= 20) {
-        message = "Do you want to draw a new card?"
-    } else if (sum === 21) {
-        message = "You've got Blackjack!"
-        hasBlackJack = true
-    } else {
-        message = "You're out of the game!"
-        isAlive = false
-    }
-    // 2. Display the message in the messageEl using messageEl.textContent
-    messageEl.textContent = message;   
+  if (sum <= 20) {
+    message = 'Do you want to draw a new card?';
+    sumEl.textContent = `Sum: ${sum}`;
+  } else if (sum === 21) {
+    message = "You've got Blackjack!";
+    sumEl.textContent = `Sum: ${sum}`;
+    hasBlackJack = true;
+  } else {
+    message = "You're out of the game!";
+    sumEl.textContent = `Sum: ${sum}`;
+    isAlive = false;
+  }
+  // 2. Display the message in the messageEl using messageEl.textContent
+  messageEl.textContent = message;
 }
-
