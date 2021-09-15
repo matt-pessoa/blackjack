98817083ede301f071a805a78475d0bddc84f704
Matheus Pessoa <matheuspessoafonseca@gmail.com>
2021-09-15 10:27:55 -0300

    Cria o botao Start Game HTML, o estiliza CSS e adiciona um event listener de clique para executar a funçao startGame() em JS

--- index.js
+++ index.js
@@ -12,12 +12,18 @@ let isAlive = true;
 
 let message = '';
 
+const startBtn = document.getElementById('start-btn');
+startBtn.addEventListener('click', startGame)
+
+function startGame() {
 if (sum <= 20) {
-  message = 'Do you want to draw a new card?';
+    message = "Do you want to draw a new card? 🙂"
 } else if (sum === 21) {
-  message = "Wohoo! You've got Blackjack!";
-  hasBlackJack = true;
+    message = "Wohoo! You've got Blackjack! 🥳"
+    hasBlackJack = true
 } else {
-  message = "You're out of the game!";
-  isAlive = false;
+    message = "You're out of the game! 😭"
+    isAlive = false
+}
+console.log(message)    
 }
